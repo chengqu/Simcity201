39f6996df933725b6f1154638fa99c5ebbbd16c5
ivandavid14 <davidiva@usc.edu>
2013-11-14 17:16:34 -0800

    added the renter and owners classes

---
+++ src/agents/ApartmentOwner.java
@@ -0,0 +1,13 @@
+package agents;
+
+import agent.Agent;
+
+public class ApartmentOwner extends Agent{
+
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
