a8a7c229afc2357c9a1c0e3af8f36c0a5fb937ea
faskowit <faskowit@usc.edu>
2013-11-18 19:40:07 -0800

    added some code to MarketCustomer...

--- src/agents/MarketCustomerAgent.java
+++ src/agents/MarketCustomerAgent.java
@@ -15,6 +15,22 @@ public class MarketCustomerAgent extends Person {
 	 * DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA 
 	 */
 	
+	List<String> whatIWant; 
+	
+	MarketEmployeeAgent employee;
+	
+	enum CustomerState {enteringStore, waiting, ordering, waitingForOrder, orderRecieved,
+		payingForOrder, paidForOrder};
+		
+	CustomerState state = CustomerState.enteringStore;
+	CustomerBill bill = new CustomerBill(); 
+	
+	//filled-in at initiation if there is debt 
+	class CustomerBill {
+	        public float charge_;
+	        public float inDebt_; 
+	        public boolean owesMoney;
+	}
 	
 	
 	// DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA DATA 
@@ -23,7 +39,29 @@ public class MarketCustomerAgent extends Person {
 	 *  MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES 
 	 */
 	
-	
+	//from Employee
+	private void AskForCustomerOrder() {
+		state = CustomerState.ordering;
+		moveToOrderingArea() //animation 
+
+	}
+
+	//from Employee 
+
+	private void HereIsYourStuff(List<String> orderList) {
+		if (orderList.get(0) == whatIwant) 
+			state = CustomerState.orderRecieved;
+	}
+
+	//from employee
+	private void HereIsYourOrderCharge(float charge) {
+		bill.charge_ = charge;
+	}
+
+	//from Employee
+	private void PaymentNoted() {
+		state = CustomerState.paidForOrder;
+	}
 	
 	// MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES MESSAGES 
 	
@@ -33,7 +71,18 @@ public class MarketCustomerAgent extends Person {
 	
 	public boolean pickAndExecuteAnAction() {
 		
-		
+		//If state == paidForOrder, then 
+			//LeaveRestaurant();
+
+		//If state == orderReceived && bill.charge != null, then 
+			//MakePayment();
+
+		//If state == ordering, then 
+			//GiveEmployeeMyOrder();
+
+		//If state == enteringStore, then
+			//State == waiting ;
+			//Employee.IamYouCustomer(this);
 		
 		return false;
 	}
@@ -44,7 +93,21 @@ public class MarketCustomerAgent extends Person {
 	 * ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS 
 	 */
 	
-	
+	private void GiveEmployeeMyOrder() {
+		state = CustomerState.waitingForOrder;
+		Employee.IWantThisStuff(deepCopyThisIWant());
+	}
+
+	private void MakePayment() {
+		state = CustomerState.payingForOrder;
+		bill.calculatePayment();
+		employee.HereIsMyMoney(bill.whatIWillPay)
+	}
+
+	private void LeaveRestaurant() {
+		DoLeaveRestaurant(); 
+		employee.IAmLeaving(); 
+	}
 	
 	// ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS ACTIONS 
 }
