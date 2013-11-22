00fd989aef69c38f427b1885f52dfeafca1713d5
ivandavid14 <davidiva@usc.edu>
2013-11-21 17:31:13 -0800

    added specific task enum to Task class

--- src/agents/Task.java
+++ src/agents/Task.java
@@ -1,14 +1,19 @@
 package agents;
 
+import java.util.*;
+
 public class Task {
 	public enum Objective{goTo, patron, worker, master}; //master refers to home
 	private Objective objective;
 	private String location;
+	public enum specificTask{eat, takeOutMoney, none};
+	private List<specificTask> specificTasks;
 	
-	public Task(Objective o, String l)
+	public Task(Objective o, String l, List<specificTask> listOfTasks)
 	{
 		objective = o;
 		location = l;
+		specificTasks = listOfTasks;
 	}
 	
 	public Objective getObjective()
@@ -19,5 +24,10 @@ public class Task {
 	public String getLocation()
 	{
 		return location;
+	}
+	
+	public List<specificTask> getTaskList()
+	{
+		return specificTasks;
 	}
 }
